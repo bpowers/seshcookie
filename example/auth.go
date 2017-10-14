51e7fc919104291b704f8ed9fefef1196d451720
Bobby Powers <bobbypowers@gmail.com>
2017-10-14 16:09:30 -0400

    update copyright date

--- example/auth.go
+++ example/auth.go
@@ -1,12 +1,13 @@
-// Copyright 2011 Bobby Powers. All rights reserved.
+// Copyright 2017 Bobby Powers. All rights reserved.
 // Use of this source code is governed by the MIT
 // license that can be found in the LICENSE file.
 package main
 
 import (
-	"github.com/bpowers/seshcookie"
 	"log"
 	"net/http"
+
+	"github.com/bpowers/seshcookie"
 )
 
 var contentDir http.Dir = "./secured"
